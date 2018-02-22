59e31107d1ac19d1093d3a84482791ee09a44b27
Stepan <eremeevst@gmail.com>
2018-02-22 17:49:38 +0400

    bird configs

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
 $(document).ready(function(){
 	(function() {
-		[].slice.call( document.querySelectorAll( '.test_svg' ) ).forEach( function( el ) {
+		[].slice.call( document.querySelectorAll( '.test_svg_arrow' ) ).forEach( function( el ) {
 			var svgicon = new svgIcon( el, svgIconConfig, { size : { w : 98.99495, h : 56.56854 } } );
+		});
+		[].slice.call( document.querySelectorAll( '.test_svg_bird' ) ).forEach( function( el ) {
+			var svgicon = new svgIcon( el, svgIconConfig, { size : { w : 370.5, h : 50.5 } } );
 		});
 	})();
 });
