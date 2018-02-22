d4a0c9b9842c2df16e2ee8fa22d474ffd87c5d2e
genotype <design2@iswin.ru>
2018-02-22 18:40:03 +0400

    animation on mouseover

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
 			var svgicon = new svgIcon( el, svgIconConfig, { size : { w : 98.99495, h : 56.56854 } } );
 		});
 		[].slice.call( document.querySelectorAll( '.test_svg_bird' ) ).forEach( function( el ) {
-			var svgicon = new svgIcon( el, svgIconConfig, { size : { w : 370.5, h : 50.5 } } );
+			var svgicon = new svgIcon( el, svgIconConfig, { size : { w : 370.5, h : 50.5 }, evtoggle: 'mouseover' } );
 		});
 	})();
 });
